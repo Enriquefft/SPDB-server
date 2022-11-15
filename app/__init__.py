1e6719732e22893a8909c6d110eb3c9947fc914e
Enriquefft <enriquefft2001@gmail.com>
2022-11-14 19:26:38 -0500

    create tables

--- app/__init__.py
+++ app/__init__.py
@@ -14,6 +14,7 @@ def create_app(config_class='Dev'):
 
     from app.models import db
     db.init_app(app)
+    db.create_all()
 
     from flask_jwt_extended import JWTManager
     jwt = JWTManager(app)
